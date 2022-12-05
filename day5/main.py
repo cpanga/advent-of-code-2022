07ec584d19cdc64b69b4b0ad4667d36b0be03502
Chris Pangalos <chrispangalos96@gmail.com>
2022-12-05 18:33:09 +0000

    Day 5 full solution

--- day5/main.py
+++ day5/main.py
@@ -5,13 +5,36 @@ def main():
 
     instructions = readfile("./day5/input.txt")
 
+    part1(instructions)
+    part2(instructions)
+
+
+def part2(instructions: str):
     crates = list([])
     buildCrates(crates)
 
-    part1(instructions, crates)
+    for line in instructions:
+        tup = processInstructions(line)
+        # Get crates to move in separate list
+        i=0
+        cratesToMove = []
+        # If moving more crates than max number of crates, move max number instead
+        if len(crates[tup[1]-1]) < (tup[0]):
+            tup[0] = len(crates[tup[1]-1])
+        while i < tup[0]:
+            cratesToMove.append(crates[tup[1]-1].pop())
+            i +=1
+        cratesToMove.reverse()
 
+        # Add crates to destination
+        for item in cratesToMove:
+            crates[tup[2]-1].append(item)
 
-def part1(instructions: str, crates: list([])):
+    print(f"Part 2 answer: {cratesOnTop(crates)}")
+
+def part1(instructions: str):
+    crates = list([])
+    buildCrates(crates)
     for line in instructions:
         tup = processInstructions(line)
         i=0
@@ -20,14 +43,18 @@ def part1(instructions: str, crates: list([])):
         while i < tup[0]:
             crates[tup[2]-1].append(crates[tup[1]-1].pop())
             i +=1
+    print(f"Part 1 answer: {cratesOnTop(crates)}")
 
-    part1 = ""
+# Return string of crates on top
+def cratesOnTop(crates: list([])):
+    topCrates = ""
     for crate in crates:
         if len(crate) > 0:
-            part1 += (crate[len(crate)-1])
-    print(f"Part 1 answer: {part1}")
+            topCrates += (crate[len(crate)-1])
+    return topCrates
 
 
+# Process instructions in to list of numbers
 def processInstructions(instr: str):
     if len(instr) == 18:
         moves = instr[5]
@@ -38,6 +65,7 @@ def processInstructions(instr: str):
 
     return (int(moves), int(start), int(instr[-1]))
 
+# Build the crates as shown in the problem description
 def buildCrates(crates: list):
     crates.append(['R','P','C','D','B','G'])
     crates.append(['H','V','G'])
